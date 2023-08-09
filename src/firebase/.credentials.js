71d38e7d2fb8da7fa139dd407b047bbb6879a6f2
youjin kim <gracecancho@gmail.com>
2023-08-09 16:19:54 +0900

    test

---
+++ src/firebase/.credentials.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBznniKpMvu9t71WYNDxoPeKjNddEHBKvg",
+    authDomain: "my-portfolio-yj.firebaseapp.com",
+    projectId: "my-portfolio-yj",
+    storageBucket: "my-portfolio-yj.appspot.com",
+    messagingSenderId: "49896736950",
+    appId: "1:49896736950:web:f8539866f41bffa8103e17",
+    measurementId: "G-HPC4G30RR1"
+};
