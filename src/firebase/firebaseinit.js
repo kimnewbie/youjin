99784c5e2ef147fb1d2c913804213ce4d1b4ccb2
youjin kim <gracecancho@gmail.com>
2023-08-11 15:29:23 +0900

    test

--- src/firebase/firebaseinit.js
+++ src/firebase/firebaseinit.js
@@ -1,10 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { firebaseConfig } from "./.credentials";
-import { getFirestore } from "firebase/firestore";
+import { connectAuthEmulator, getAuth } from "firebase/auth";
+import { connectFirestoreEmulator, getFirestore } from "firebase/firestore";
+import { connectFunctionsEmulator, getFunctions } from "firebase/functions";
 
 // Firebase를 초기화 및 Firebase 앱 객체 생성
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-export const db = getFirestore(app); 
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const fbFunctions = getFunctions(app);
+
+//
+if (process.env.NODE_ENV === 'development') {
+    connectAuthEmulator(auth, 'http://localhost:9099');
+    connectFirestoreEmulator(db, "localhost", 8080);
+    connectFunctionsEmulator(fbFunctions, "localhost", 5001);
+}
